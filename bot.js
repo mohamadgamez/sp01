2eaf3a133e3cc1e1e6828c4c0806914ea63250ee
mohamadgamez <46826596+mohamadgamez@users.noreply.github.com>
2019-02-23 21:22:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("548938316486279168")
 setInterval(function() {
 channel.send(`spam-by-mody spam-by-mody spam-by-mody spam-by-mody`);
-}, 30)
+}, 30000000)
 })
 
 client.login(process.env.BOT_TOKEN);
